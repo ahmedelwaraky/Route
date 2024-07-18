0d7f365bbf1d916b015c6c80e23c8d79ccc4ad13
Ahmed Elwaraky <ahmed.elwaraky1@gmail.com>
2024-07-18 20:32:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import TransactionGraph from './components/TransactionGraph/TransactionGraph'
+import CustomerTable from './components/CustomerTable/CustomerTable'
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
+import Home from './components/Home/Home';
+import Notfound from './components/Notfound/Notfound';
+import TransactionsWithLocalServer from './components/TransactionsWithLocalServer/TransactionsWithLocalServer';
+
+const router=createBrowserRouter([
+{path:'',element:<Layout/>,children:[
+{path:'',element: <ProtectedRoute><Home/></ProtectedRoute>},
+{path:'home',element:<ProtectedRoute><Home/></ProtectedRoute>},
+{path:'customer',element:<ProtectedRoute><CustomerTable/></ProtectedRoute>},
+{path:'transaction',element:<ProtectedRoute><TransactionGraph/></ProtectedRoute>},
+{path:'transactionwithlocalserver',element:<ProtectedRoute><TransactionsWithLocalServer/></ProtectedRoute>},
+{path:'*',element:<Notfound/>}
+]
+}
+])
+function App() {
+
+  return (
+   
+    <RouterProvider router={router}></RouterProvider>
+   
+  );
+}
+
+export default App;
